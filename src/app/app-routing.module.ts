72d481fbf467bbdffaad3e9977b1cda5feceb6c3
Jester Jay <justine.carbonel.92@gmail.com>
2021-04-12 20:09:00 +0800

    search component delete and nav updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,12 @@ import { HomeComponent } from './home/home.component'
 import { AboutComponent } from './about/about.component'
 import { ServicesComponent } from './services/services.component'
 import { ProgramsComponent } from './programs/programs.component'
-import { SearchComponent } from './search/search.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'programs', component: ProgramsComponent },
-  { path: 'search', component: SearchComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
 import { ProgramsComponent } from './programs/programs.component';
-import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +15,6 @@ import { SearchComponent } from './search/search.component';
     AboutComponent,
     ServicesComponent,
     ProgramsComponent,
-    SearchComponent
   ],
   imports: [
     BrowserModule,
